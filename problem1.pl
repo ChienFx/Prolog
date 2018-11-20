7e9bc6c3d63089ff2ea8775d72dec3ac3267f417
chienfx <chienpham304@gmail.com>
2018-11-20 12:26:30 +0700

    problem 1: insertd fact

---
+++ problem1.pl
@@ -0,0 +1,107 @@
+%Married
+married('Queen Elizabeth II', 'Prince Phillip').
+married('Prince Phillip', 'Queen Elizabeth II').
+
+married('Prince Charles', 'Camilla Parker Bowles').
+married('Camilla Parker Bowles', 'Prince Charles').
+
+married('Princess Anne', 'Timothy Laurence').
+married('Timothy Laurence', 'Princess Anne').
+
+married('Sophie Rhys-jones', 'Prince Edward').
+married('Prince Edward', 'Sophie Rhys-jones').
+
+married('Prince William', 'Kate Middleton').
+married('Kate Middleton', 'Prince William').
+
+married('Autumn Kelly', 'Peter Phillips').
+married('Peter Phillips', 'Autumn Kelly').
+
+married('Zara Phillips', 'Mike Tindall').
+married('Mike Tindall', 'Zara Phillips').
+
+%Divorced
+divorced('Princess Diana', 'Prince Charles').
+divorced('Prince Charles', 'Princess Diana').
+
+divorced('Captain Mark Phillips', 'Princess Anne').
+divorced('Princess Anne', 'Captain Mark Phillips').
+
+divorced('Sarah Ferguson', 'Prince Andrew').
+divorced('Prince Andrew', 'Sarah Ferguson').
+
+%Parent
+parent('Queen Elizabeth II', 'Prince Charles').
+parent('Queen Elizabeth II', 'Princess Anne').
+parent('Queen Elizabeth II', 'Prince Andrew').
+parent('Queen Elizabeth II', 'Prince Edward').
+parent('Prince Phillip', 'Prince Charles').
+parent('Prince Phillip', 'Princess Anne').
+parent('Prince Phillip', 'Prince Andrew').
+parent('Prince Phillip', 'Prince Edward').
+
+
+parent('Princess Diana', 'Prince William').
+parent('Princess Diana', 'Prince Harry').
+parent('Prince Charles', 'Prince William').
+parent('Prince Charles', 'Prince Harry').
+
+parent('Captain Mark Phillips', 'Peter Phillips').
+parent('Captain Mark Phillips', 'Zara Phillips').
+parent('Princess Anne', 'Peter Phillips').
+parent('Princess Anne', 'Zara Phillips').
+
+parent('Sarah Ferguson', 'Princess Beatrice').
+parent('Sarah Ferguson', 'Princess Beatrice').
+parent('Prince Andrew', 'Princess Eugenie').
+parent('Prince Andrew', 'Princess Eugenie').
+
+parent('Sophie Rhys-jones', 'James Viscount Severn').
+parent('Sophie Rhys-jones', 'Lady Louise Mountbatten-Windsor').
+parent('Prince Edward', 'James Viscount Severn').
+parent('Prince Edward', 'Lady Louise Mountbatten-Windsor').
+
+parent('Prince William', 'Prince George').
+parent('Prince William', 'Princess Charlotte').
+parent('Kate Middleton', 'Prince George').
+parent('Kate Middleton', 'Princess Charlotte').
+
+parent('Autumn Kelly', 'Savannah Phillips').
+parent('Autumn Kelly', 'Isla Phillips').
+parent('Peter Phillips', 'Savannah Phillips').
+parent('Peter Phillips', 'Isla Phillips').
+
+parent('Zara Phillips', 'Mia Grace Tindall').
+parent('Mike Tindall', 'Mia Grace Tindall').
+
+%male
+male('Prince Phillip').
+male('Prince Charles').
+male('Captain Mark Phillips').
+male('Timothy Laurence').
+male('Prince Andrew').
+male('Prince Edward').
+male('Prince William').
+male('Prince Harry').
+male('Peter Phillips').
+male('Mike Tindall').
+male('James Viscount Severn').
+male('Prince George').
+
+%female
+female('Queen Elizabeth II').
+female('Prince Diana').
+female('Camilla Parker Bowles').
+female('Prince Anne').
+female('Sarah Ferguson').
+female('Sophie Rhys-jones').
+female('Kate Middleton').
+female('Autumn Kelly').
+female('Zara Phillips').
+female('Princess Beatrice').
+female('Princess Eugenie').
+female('Lady Louise Mountbatten-Windsor').
+female('Princess Charlotte').
+female('Savannah Phillips').
+female('Isla Phillips').
+female('Mia Grace Tindall').
